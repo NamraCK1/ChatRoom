55d24005fbfadf567280f586e14378380eb05020
NamraCK1 <namra.coderkube@gmail.com>
2024-01-29 12:34:22 +0530

    just upload

---
+++ resources/views/register.blade.php
@@ -0,0 +1,6 @@
+<form action="" method="post">
+    @csrf
+    <input type="text" name="name" placeholder="name" required /><br>
+    <input type="submit"  /><br>
+
+</form>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Events\UserEvent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/register', function() {
+    return view('register');
+});
+
+Route::post('/register', function() {
+    $name = request()->name;
+    
+    event(new UserEvent($name));
+    
+});
